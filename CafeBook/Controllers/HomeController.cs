920a8193f5e69ae0e0bfbef496f2869cc2631eb8
ablinaa <azharabks@gmail.com>
2019-12-14 01:03:42 +0600

    front

--- CafeBook/Controllers/HomeController.cs
+++ CafeBook/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CafeBook.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,20 @@ namespace CafeBook.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly BookService _bookService;
+        private readonly BookTypeService _bookTypeService;
+
+        public HomeController(BookService bookService, BookTypeService bookTypeService)
         {
-            return View();
+            _bookService = bookService;
+            _bookTypeService = bookTypeService;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var bookTypes = await _bookTypeService.GetBookTypes();
+            return View(bookTypes);
         }
     }
 }
